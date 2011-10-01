d3a0e43979441dd5fae1aab83b72ee77166525de
Brett Williams <brett@brettandbecky.net>
2011-10-01 14:38:07 -0600

    Added script

---
+++ scripts/strongbad_backup_to_external.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rsync_cmd="rsync"
+rsync_cmd="echo rsync"
+rsync_opts="-rtLgpv --delete" 
+dstdir_base="/media/HITACHI/StrongbadBackup"
+
+# backup_dirs uses --delete, so this is something that the main
+# copy is on the host, not the external drive
+declare -A backup_dirs
+backup_dirs["/mnt/vol/mp3"]="${dstdir_base}/mp3"
+backup_dirs["/mnt/vol/video"]="${dstdir_base}/video"
+backup_dirs["/mnt/data/pictures"]="${dstdir_base}/pictures"
+backup_dirs["/mnt/data/sound_effects"]="${dstdir_base}/sound_effects"
+backup_dirs["/mnt/data/video_game_music"]="${dstdir_base}/video_game_music"
+backup_dirs["/mnt/data/Games/Emulation"]="${dstdir_base}/Emulation"
+
+for bdir in "${!backup_dirs[@]}"; do 
+  $rsync_cmd $rsync_opts $bdir/ ${backup_dirs["$bdir"]}
+done
