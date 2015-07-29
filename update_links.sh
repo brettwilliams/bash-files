68383cf39c5e8cb3cea91f05f25ed078663b2fb1
Brett Williams <brett@brettandbecky.net>
2015-07-29 13:58:56 -0600

    Make ssh updates too

--- update_links.sh
+++ update_links.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-
+# First, everything is in home directory, cd to it
 cd
-rsync_files=".ag_user_info .pager.ini .gitconfig"
 
+# SSH keys must be correct.  Sync up if it doesn't look right
+if [[ ! -d .ssh || $(md5sum .ssh/id_rsa.pub | awk '{print $1}') != "ac9a00ee80b3a473f5db8d62ee00ea61" ]]; then
+  echo "INFO: Redoing your .ssh directory, it does not look correct"
+  rm -rf .ssh
+  rsync -az -e ssh darkshore.ftc.avagotech.net:/net/easystreet/vol/homes/brettw/.ssh .
+fi
+
+# Files that just need to be rsynced
+rsync_files=".ag_user_info .pager.ini .gitconfig"
 for f in $rsync_files; do
   if [[ ! -f $f ]]; then
     echo scp brettw@darkshore.ftc.avagotech.net://net/easystreet/vol/homes/brettw/$f .
