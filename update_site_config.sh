0ce91a3693662404607785c8652754fafcea9d98
Brett Williams <brett@brettandbecky.net>
2016-02-16 08:56:51 -0700

    update with Keiths suggestions

--- update_site_config.sh
+++ update_site_config.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # First, everything is in home directory, cd to it
-cd
+cd $HOME || exit 1
 
 # Update this script!  If no argument given, just update here and exec
 # self with an argument
 if [[ -z "$1" ]]; then
   echo "INFO:  first time, update the update!"
   cd $HOME/github/bash-files
+  # TODO:  if no github/bash-files, clone it, clone the other two to bootstrap
   git pull
   cd $HOME
   echo "INFO: execing self"
@@ -14,31 +15,22 @@ if [[ -z "$1" ]]; then
 fi
 echo "INFO: actually doing the sync"
 
-# SSH keys must be correct.  Sync up if it doesn't look right
-# if [[ ! -d .ssh || $(md5sum .ssh/id_rsa.pub | awk '{print $1}') != "ac9a00ee80b3a473f5db8d62ee00ea61" ]]; then
-#   echo "INFO: Redoing your .ssh directory, it does not look correct"
-#   rm -rf .ssh
-#   rsync -az -e ssh darkshore.ftc.avagotech.net:/net/easystreet/vol/homes/brettw/.ssh .
-# fi
-
 git_dirs=(
 $HOME/github/bash-files
 $HOME/github/fluxbox-files
 $HOME/github/vim-files
 )
-# $HOME/github/config-files
 
 for git_dir in ${git_dirs[*]}; do
   cd $git_dir
   git pull
 done
 cd $HOME
+
 # Files that just need to be rsynced
 rsync_files=".ssh .ag_user_info .pager.ini .gitconfig .wizardrc .wizard_post_hook .vnc"
 for f in $rsync_files; do
-#  if [[ ! -f $f ]]; then
-    rsync -a -e ssh brettw@darkshore.ftc.avagotech.net://net/easystreet/vol/homes/brettw/$f .
-#  fi
+  rsync -a -e ssh brettw@darkshore.ftc:/home/brettw/$f .
 done
 
 # Don't let these get out of sync!
@@ -68,7 +60,8 @@ idx=0
 for src_file in ${link_srcs[*]}; do
   dst_file=${link_dsts[$idx]}
   if [[ ! -e $dst_file || $(readlink $dst_file) != $src_file ]]; then
-    ln -sf $src_file $dst_file
+    rm -f $dst_file
+    ln -s $src_file $dst_file
   fi
   idx=$(( $idx + 1 ))
 done
